2beb907ed6b3455ce3b47e4b063c0ca0399b87cc
NamitaSharma21 <namitasharmams_cse23@its.edu.in>
2026-07-02 14:18:24 +0530

    file changed

--- Backend/app.js
+++ Backend/app.js
@@ -18,6 +18,7 @@ const app = express();
 app.use(helmet());
 
 // CORS configuration
+console.log("CLIENT_URL =", process.env.CLIENT_URL);
 app.use(cors({
   origin: process.env.CLIENT_URL || 'http://localhost:5173',
   credentials: true,
